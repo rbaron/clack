6283849d3e9e0be5f05edd1890b5c5d688a7b4ca
Raphael Baron <rbaron@rbaron.net>
2017-06-18 22:09:38 -0300

    Cleaned up example

--- src/clack/example.clj
+++ src/clack/example.clj
@@ -4,27 +4,23 @@
             [environ.core :refer [env]])
   (:gen-class))
 
-(clack/set-log-level! :info)
-
 (defn send-ack
   [msg out-chan my-user-id]
   (if (and (= (:type msg) "message")
            (not= (:user my-user-id) my-user-id))
     (async/go (async/>! out-chan {:type "message"
-                        :channel (:channel msg)
-                        :text "Ok!"}))))
+                                  :channel (:channel msg)
+                                  :text "Ok!"}))))
 
 (defn handler
   [in-chan out-chan config]
-  (let [{:keys [my-user-id websocket-url]} config]
-    (async/go-loop
-      []
-      (if-let [msg (async/<! in-chan)]
-        (do
-          (println "Got msg" msg)
-          (send-ack msg out-chan my-user-id)
-          (recur))
-        (println "Channel is closed")))))
+  (async/go-loop []
+    (if-let [msg (async/<! in-chan)]
+      (do
+        (println "Got msg" msg)
+        (send-ack msg out-chan (:my-user-id config))
+        (recur))
+      (println "Channel is closed"))))
 
 (defn -main
   [& args]
